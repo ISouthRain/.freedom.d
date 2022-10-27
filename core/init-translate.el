9a8d2b866116e6e6cbad24e8e211557c2953d585
ISouthRain <65887663+ISouthRain@users.noreply.github.com>
2022-10-27 17:20:03 +0800

    go-translate

--- core/init-translate.el
+++ core/init-translate.el
@@ -1,3 +1,36 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package go-translate
+  ;; :defer-incrementally t
+  :ensure t
+  :commands (gts-do-translate)
+  :config
+  ;; 配置多个翻译语言对
+  (setq gts-translate-list '(("en" "zh") ("fr" "zh")))
+  ;; 设置为 t 光标自动跳转到buffer
+  (setq gts-buffer-follow-p t)
+  ;; (if (display-graphic-p)
+  ;;     (if (posframe-workable-p)
+  ;;         (setq gts-default-translator
+  ;;               (gts-translator
+  ;;                :picker (gts-noprompt-picker)
+  ;;                :engines (list (gts-google-rpc-engine) (gts-bing-engine))
+  ;;                :render (gts-posframe-pop-render :forecolor "#ffffff" :backcolor "#111111")))
+  ;;       ;; :render (gts-posframe-pin-render :width 40 :height 15 :position (cons 1500 20) :forecolor "#ffffff" :backcolor "#111111")))
+  ;;       )
+
+  ;;   (setq gts-default-translator
+  ;;         (gts-translator
+  ;;          :picker (gts-noprompt-picker)
+  ;;          :engines (list (gts-google-rpc-engine) (gts-bing-engine))
+  ;;          :render (gts-buffer-render)))
+  ;;   )
+  (gts-translator
+   :picker (gts-noprompt-picker)
+   :engines (list (gts-google-rpc-engine) (gts-bing-engine))
+   :render (gts-buffer-render))
+
+  );; go-translate
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package corfu-english-helper
   :ensure nil
   :defer 0.5
