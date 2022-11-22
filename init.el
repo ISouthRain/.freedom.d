acc2d1631582f5e0a4ae5169f618dd5ac7fc65f7
ISouthRain <isouthrain@gmail.com>
2022-11-22 20:24:19 +0800

    Initial commit

--- init.el
+++ init.el
@@ -1,3 +1,8 @@
-(add-to-list 'load-path "~/.freedom.d")
+(setq gc-cons-threshold (* 100 1024 1024))
 
-(require 'README)
+(if (file-exists-p (expand-file-name "~/.freedom.d/config.el"))
+    (load-file (expand-file-name "~/.freedom.d/config.el"))
+  (org-babel-load-file (expand-file-name "~/.freedom.d/config.org")))
+
+;; Make gc pauses faster by decreasing the threshold.
+(setq gc-cons-threshold (* 10 1000 1000))
